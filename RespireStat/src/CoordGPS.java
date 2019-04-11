ad0f4db14fecffa733796363eca3222a6c15ae18
Alexandre_MEYER <a.meyer@ecole-ipssi.net>
2019-04-11 17:47:16 +0200

    Attributs CoordGPS

--- RespireStat/src/CoordGPS.java
+++ RespireStat/src/CoordGPS.java
@@ -1,4 +1,13 @@
 
 public class CoordGPS {
-
+	private double latitude;
+	private double longitude;
+	
+	public CoordGPS(String s) {
+		
+	}
+	public CoordGPS(double l, double L) {
+		latitude = l;
+		longitude = L;
+	}
 }
