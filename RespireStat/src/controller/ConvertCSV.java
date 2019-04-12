2822e8d67a0d4dc5a02ff375904e4df65531030d
Alexandre_MEYER <a.meyer@ecole-ipssi.net>
2019-04-12 10:11:59 +0200

    Mise à jour lire de ConvertCSV

--- RespireStat/src/controller/ConvertCSV.java
+++ RespireStat/src/controller/ConvertCSV.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import com.opencsv.CSVReader;
 
 public class ConvertCSV {
@@ -9,7 +10,16 @@ public class ConvertCSV {
 	public static void lire(String fileName) {
 			try {
 				CSVReader reader = new CSVReader(new FileReader(fileName));
+				String[] nextLine = reader.readNext();
+				while((nextLine = reader.readNext ()) != null) {
+					for(int i= 0; i<nextLine.length; i++) {
+						System.out.print(nextLine[i] + " ");
+					}
+					System.out.println();
+				}
 			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 	}
