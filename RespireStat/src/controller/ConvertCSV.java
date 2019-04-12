15c71098b8b8be498d67dd0a801b4c1ada575bec
Alexandre_MEYER <a.meyer@ecole-ipssi.net>
2019-04-12 10:01:29 +0200

    Mise à jour ConvertCSV

--- RespireStat/src/controller/ConvertCSV.java
+++ RespireStat/src/controller/ConvertCSV.java
@@ -1,8 +1,16 @@
 package controller;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import com.opencsv.CSVReader;
+
 public class ConvertCSV {
 	
 	public static void lire(String fileName) {
-			CVSReader reader = new CSVReader(new FileReader(""));
+			try {
+				CSVReader reader = new CSVReader(new FileReader(fileName));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
 	}
 }
