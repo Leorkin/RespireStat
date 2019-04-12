cd9f207870fb343511cab0bded67bc8e1f3d50d7
utilisateur <utilisateur@ASUS-STRIX>
2019-04-12 16:51:26 +0200

    Ajout variable Etablissement

--- RespireStat/src/model/Etablissement.java
+++ RespireStat/src/model/Etablissement.java
@@ -3,9 +3,13 @@ package model;
 public class Etablissement {
 	private String identifiant;
 	private String nomEtablissement;
+	private Lieu lieuEtablissement;
+	private Type typeEtablissement;
+	private CoordGPS coordonneesEtablissement;
 	
 	public Etablissement(String[] nextLine) {
 		identifiant = nextLine[0];
 		nomEtablissement = nextLine[1];
+		
 	}
 }
